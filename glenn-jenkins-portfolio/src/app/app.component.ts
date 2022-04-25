4199398bad2bf4c405b4e26cd01e0ddde7495966
jenkins1085 <jenkins.1085@osu.edu>
2022-04-25 14:30:56 -0400

    renamed glenn images, added script ohio photo to home page

--- glenn-jenkins-portfolio/src/app/app.component.ts
+++ glenn-jenkins-portfolio/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <app-header></app-header>
+    <app-home></app-home>
     <router-outlet></router-outlet>
     <app-footer></app-footer>
   `,
--- glenn-jenkins-portfolio/src/app/home/home.component.ts
+++ glenn-jenkins-portfolio/src/app/home/home.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   template: `
-    <p>
-      home works!
-    </p>
+    <section class="hero is-bold is-halfheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Glenn Jenkins Portfolio Website</h1>
+        </div>
+      </div>
+      <div class="faded">
+        <figure class="image is-16by9">
+          <img src="/assets/images/glenn/osu-script.jpg" alt="Script Ohio" />
+        </figure>
+      </div>
+    </section>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
